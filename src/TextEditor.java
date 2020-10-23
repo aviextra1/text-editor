edb10535c8ec01a20a036ad16401a4db6c364f9d
aviextra1 <73329841+aviextra1@users.noreply.github.com>
2020-10-23 12:08:41 +0530

    Add files via upload

---
+++ src/TextEditor.java
@@ -0,0 +1,145 @@
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class TextEditor extends JFrame implements ActionListener {
+
+    JTextArea textArea;
+    JScrollPane scrollPane;
+    JSpinner fontSizeSpinner;
+    JLabel fontLabel;
+    JButton fontColorButton;
+    JComboBox fontBox;
+    JMenuBar menuBar;
+    JMenu fileMenu;
+    JMenuItem openItem;
+    JMenuItem saveItem;
+    JMenuItem exitItem;
+
+    TextEditor(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("TextEditor");
+        this.setSize(1980,1100);
+        this.setLayout(new FlowLayout());
+        this.setLocationRelativeTo(null);
+        textArea = new JTextArea();
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setFont(new Font("Arial",Font.PLAIN,72));
+
+        scrollPane = new JScrollPane(textArea);
+        scrollPane.setPreferredSize(new Dimension(1900,1050));
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+        fontLabel = new JLabel("Font: ");
+
+
+        fontSizeSpinner = new JSpinner();
+        fontSizeSpinner.setPreferredSize(new Dimension(50,25));
+        fontSizeSpinner.setValue(20);
+        fontSizeSpinner.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent changeEvent) {
+                textArea.setFont(new Font(textArea.getFont().getFamily(),Font.PLAIN,(int)fontSizeSpinner.getValue()));
+            }
+        });
+        fontColorButton = new JButton("Color");
+        fontColorButton.addActionListener(this);
+
+        String[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+        fontBox = new JComboBox(fonts);
+        fontBox.addActionListener(this);
+        fontBox.setSelectedItem("Arial");
+
+        menuBar = new JMenuBar();
+        fileMenu = new JMenu("File");
+        openItem = new JMenuItem("Open");
+        saveItem = new JMenuItem("Save");
+        exitItem = new JMenuItem("Exit");
+
+        openItem.addActionListener(this);
+        saveItem.addActionListener(this);
+        exitItem.addActionListener(this);
+
+        fileMenu.add(openItem);
+        fileMenu.add(saveItem);
+        fileMenu.add(exitItem);
+        menuBar.add(fileMenu);
+
+        this.setJMenuBar(menuBar);
+        this.add(fontLabel);
+        this.add(fontSizeSpinner);
+        this.add(fontColorButton);
+        this.add(fontBox);
+        this.add(scrollPane);
+        this.setVisible(true);
+    }
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        if(actionEvent.getSource() == fontColorButton){
+            JColorChooser colorChooser = new JColorChooser();
+            Color color = colorChooser.showDialog(null,"Choose a color",Color.black);
+            textArea.setForeground(color);
+        }
+        if(actionEvent.getSource()==fontBox){
+            textArea.setFont(new Font((String)fontBox.getSelectedItem(),Font.PLAIN,textArea.getFont().getSize()));
+        }
+        if(actionEvent.getSource()==openItem){
+            JFileChooser fileOpener = new JFileChooser();
+            fileOpener.setCurrentDirectory(new File("."));
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("Text files","txt");
+            fileOpener.setFileFilter(filter);
+            int response1 = fileOpener.showOpenDialog(null);
+            if(response1 == JFileChooser.APPROVE_OPTION){
+                File file = new File(fileOpener.getSelectedFile().getAbsolutePath());
+                Scanner fileIn = null;
+                try{
+                    fileIn = new Scanner(file);
+                    if(file.isFile()){
+                        while(fileIn.hasNextLine()){
+                            String line = fileIn.nextLine()+"\n";
+                            textArea.append(line);
+                        }
+                    }
+                }
+                catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    fileIn.close();
+                }
+            }
+        }
+        if(actionEvent.getSource()==saveItem){
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setCurrentDirectory(new File("."));
+            int response = fileChooser.showSaveDialog(null);
+
+            if(response == JFileChooser.APPROVE_OPTION){
+                File file;
+                PrintWriter fileOut = null;
+                file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                try {
+                    fileOut = new PrintWriter(file);
+                    fileOut.println(textArea.getText());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    fileOut.close();
+                }
+            }
+        }
+        if(actionEvent.getSource()==exitItem){
+            System.exit(0);
+        }
+    }
+}
